0cc7c6ff667aa4f64bbb11d07594d8abd0342aeb
Selena Siri <selenasiri@gmail.com>
2022-11-15 13:24:45 -0500

    complete css

--- src/App.vue
+++ src/App.vue
@@ -41,19 +41,9 @@ export default {
     },
     removeGoal(idx) {
       this.goals.splice(idx, 1);
-      // splice - ?
     },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles.css" />
